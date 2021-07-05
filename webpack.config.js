7bf93f6ee8265f76f9f94ae908e91e8403cbdaaa
choongraej <choongraej@gamil.com>
2021-07-05 22:53:39 +0900

    Add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,14 @@ module.exports = {
                 test:/\.png$/,
                 use:['url-loader']
             },
+            {
+                test:/\.s[ac]ss$/i,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
         ],
     },
     plugins:[

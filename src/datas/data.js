6bbee16fce0f779014252bc97cd92cc78d98eae5
choongraej <choongraej@gamil.com>
2021-07-05 22:06:44 +0900

    Link png

--- src/datas/data.js
+++ src/datas/data.js
@@ -3,8 +3,18 @@ const data = {}
 require('./categories/group.json').forEach(g => {
     data[g] = {}
     require(`./categories/${g}.json`).forEach(c => {
-        data[g][c] = require(`./skillList/${g}/${c}.json`)
+        data[g][c] = {}
+        data[g][c].img = require(`./classImg/${c}.png`).default
+        data[g][c].skill = {}
+        var skillList = require(`./skillList/${g}/${c}.json`)
+        skillList.list.forEach(s => {
+            data[g][c].skill[s] = {}
+            data[g][c].skill[s].nick = skillList.nick[s]
+            data[g][c].skill[s].img = require(`./skillImg/${g}/${c}/${s}.png`).default
+        })
     })
 })
+
+console.log(data)
 
 export default data

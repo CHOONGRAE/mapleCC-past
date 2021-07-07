268537075234a4325fe177f28c3c2926243ea0f9
choongraej <choongraej@gamil.com>
2021-07-07 12:54:44 +0900

    Modified style for CoreList

--- src/components/CoreManagement.js
+++ src/components/CoreManagement.js
@@ -5,10 +5,12 @@ import CoreItem from './CoreItem'
 export default class CoreManagement extends React.Component {
     render = () => <Store.Consumer>
         {store => <div id='CoreManagement'>
-            <div className='list'>
-                {[...Array(35).keys()].map(v =>
-                    <CoreItem key={v} className={`r${parseInt(v / 7)} c${v % 7}`} />
-                )}
+            <div className='listBorder'>
+                <div className='list'>
+                    {[...Array(25).keys()].map(v =>
+                        <CoreItem key={v} className={`r${parseInt(v / 7)} c${v % 7}`} />
+                    )}
+                </div>
             </div>
         </div>}
     </Store.Consumer>

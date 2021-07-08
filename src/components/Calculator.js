c1c9965928a922510b1d582b5b33c7d5dfd8e16a
choongraej <choongraej@gamil.com>
2021-07-09 00:21:57 +0900

    Modified html

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,10 +19,12 @@ export default class Calculator extends React.Component {
                 superposition,
                 minCores,
                 plusCores,
+                subSkillList,
                 calcStatus,
                 _changeMode,
                 _changeSuperposition,
-                _changePlusCores
+                _changePlusCores,
+                _changeSubSkill
             } = store
 
             const status = ['계 산 시 작','계 산 중 ...(Click:취소)','계 산 완 료']
@@ -35,6 +37,27 @@ export default class Calculator extends React.Component {
                 <img src={img(skillData[core[1]])} />
                 <img src={require('../datas/iconFrame.frame3.png').default} />
             </div>
+
+            const option = () => {
+                if (targetSkillList.length > 0) {
+                    var possible = (minCores + plusCores) * 3
+                    var required = targetSkillList.length * superposition
+                    if (possible - required > 0) return <div className='option'>
+                        <div className='title inline'>
+                            &nbsp;서 브 강 화&nbsp;
+                        </div>
+                        <div className='list inline'>
+                            {skillList.filter(v => !targetSkillList.includes(v)).map(v =>
+                                <div key={v} className='item inline'>
+                                    <label htmlFor={v}>
+                                        <input type='checkbox' id={v} checked={subSkillList.includes(v)} onChange={_changeSubSkill} />
+                                        {v}
+                                    </label>
+                                </div>)}
+                        </div>
+                    </div>
+                }
+            }
 
             return <div id='Calculator'>
                 <div className='coreList'>
@@ -56,27 +79,16 @@ export default class Calculator extends React.Component {
                     </p>
                     <div className='inline' onClick={_changeSuperposition}>&nbsp;{superposition}&nbsp;중 첩&nbsp;</div>
                     <div className='inline' onClick={_changePlusCores}>
-                        &nbsp;{minCores + plusCores}&nbsp;
-                        ( 최 소{plusCores == 0 ? null : ` + ${plusCores}`} )&nbsp;코 어&nbsp;
+                        &nbsp;{minCores + plusCores}
+                        &nbsp;( 최 소
+                        {plusCores == 0 ? null : ` + ${plusCores}`} 
+                        &nbsp;) 코 어&nbsp;
                         </div>
                     <p className='inline'>
                         &nbsp;가 되도록 계산합니다.
                     </p>
                 </div>
-                <div className='option'>
-                    <div className='title inline'>
-                        &nbsp;서 브 스 킬&nbsp;
-                    </div>
-                    <div className='list inline'>
-                        {skillList.filter(v => !targetSkillList.includes(v)).map(v =>
-                            <div key={v} className='item inline'>
-                                <label htmlFor={'sub'+v}>
-                                    <input type='checkbox' id={'sub' + v} />
-                                    {v}
-                                </label>
-                            </div>)}
-                    </div>
-                </div>
+                {option()}
                 <div className='btns'>
                     <input type='button' id='home' onClick={_changeMode}/>
                     <label htmlFor='home'>돌 아 가 기</label>

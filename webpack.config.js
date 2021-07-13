5c058961ed1c08ff20ca791b9c051cc8e8c92809
choongraej <choongraej@gamil.com>
2021-07-13 15:24:25 +0900

    Add babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry:{
-        'js/app':['./src/App.jsx'],
+        'js/app':['babel-polyfill','./src/App.jsx'],
     },
     output:{
         path:path.resolve(__dirname,'dist/'),
